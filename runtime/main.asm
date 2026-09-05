94a81aa1e578f7a5b2d2011120b6a51d03f9bbb5
Jason Surguine <jaybot7@users.noreply.github.com>
2026-09-05 14:08:21 +0900

    Wire runtime to generated reset entry

--- runtime/main.asm
+++ runtime/main.asm
@@ -1,9 +1,6 @@
 ; nes2gbc Game Boy Color runtime skeleton.
-; RGBDS syntax. This is intentionally tiny until the compiler emits code.
-
 DEF rKEY1 EQU $FF4D
 DEF rLCDC EQU $FF40
-DEF rIE   EQU $FFFF
 
 SECTION "VBlank Vector", ROM0[$0040]
     reti
@@ -12,20 +9,19 @@ SECTION "Header Entry", ROM0[$0100]
     nop
     jp Start
 
-; RGBDS rgbfix fills/checks the rest of the cartridge header.
 SECTION "Runtime", ROM0[$0150]
 Start:
     di
 
-    ; Request CGB double-speed mode. STOP performs the speed switch when KEY1 bit 0 is set.
+    ; Request CGB double-speed mode.
     ld a, $01
     ldh [rKEY1 & $FF], a
     stop
 
-    ; Keep LCD disabled until generated graphics state is ready.
     xor a
     ldh [rLCDC & $FF], a
 
-.hang:
-    halt
-    jr .hang
+    jp nes_reset
+
+INCLUDE "io.asm"
+INCLUDE "generated.asm"
