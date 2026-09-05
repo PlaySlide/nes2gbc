b6deddb4d0dd37e2ab6cbe12170dcb9ad67593c5
Jason Surguine <jaybot7@users.noreply.github.com>
2026-09-05 23:33:41 +0900

    Latch host VBlank with GBC interrupt

--- runtime/main.asm
+++ runtime/main.asm
@@ -2,15 +2,27 @@
 INCLUDE "hardware.inc"
 
 SECTION "VBlank Vector", ROM0[$0040]
-    reti
+    jp nes_gbc_vblank_isr
 
 SECTION "Header Entry", ROM0[$0100]
     nop
     jp Start
 
 SECTION "Runtime", ROM0[$0150]
+nes_gbc_vblank_isr:
+    push af
+    ld a, [nes_nmi_active]
+    and a
+    jr nz, .done
+    ld a, $01
+    ld [nes_host_vblank_pending], a
+.done:
+    pop af
+    reti
+
 Start:
     di
+    ld sp, $FFFE
 
     ; Request CGB double-speed mode.
     ld a, $01
@@ -35,7 +47,7 @@ Start:
     ld [nes_dac], a
     ld [nes_controller_strobe], a
     ld [nes_controller_shift], a
-    ld [nes_nmi_vblank_seen], a
+    ld [nes_host_vblank_pending], a
     ld [nes_nmi_active], a
     ld [nes_current_code_bank], a
     ld [nes_dispatch_cache_valid], a
@@ -47,6 +59,14 @@ Start:
 
     call nes_generated_init
     call nes_video_init
+
+    ; Use the real GBC VBlank interrupt only as a one-byte event latch. The
+    ; translated NES interrupt is still delivered at compiler-selected safe points.
+    xor a
+    ldh [rIF], a
+    ld a, $01
+    ld [rIE], a
+    ei
     jp nes_reset
 
 INCLUDE "io.asm"
