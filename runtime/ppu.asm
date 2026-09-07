36cc9c670eacc9335733a65c0695ab8a1454eac3
Jason Surguine <jaybot7@users.noreply.github.com>
2026-09-07 18:23:31 +0900

    Revert regressive nametable staging experiment

--- runtime/ppu.asm
+++ runtime/ppu.asm
@@ -256,18 +256,6 @@ nes_ppu_write_data:
     call nes_ppu_map_nametable_hl
     ld a, e
     ld [hl], a
-
-    ; Never expose a partially executed NES NMI through live GBC VRAM. The
-    ; authoritative virtual nametable is updated immediately, but host VRAM is
-    ; committed only after translated RTI completes.
-    ld a, [nes_nmi_active]
-    and a
-    jr z, .nametable_sync_now
-    call nes_ppu_stage_nametable_hl
-    jp nes_ppu_increment_addr
-
-.nametable_sync_now:
-    ld a, e
     call nes_video_sync_nametable_write
     jp nes_ppu_increment_addr
 
@@ -282,42 +270,6 @@ nes_ppu_write_data:
     ld [hl], a
     call nes_video_sync_palette_write
     jp nes_ppu_increment_addr
-
-; Append physical virtual nametable address HL ($D000-$D7FF) to the
-; current NMI staging queue. The value itself already lives in authoritative WRAM.
-; Queue capacity is 1024 addresses; overflow is recorded for diagnostics/fallback.
-nes_ppu_stage_nametable_hl:
-    ld a, [nes_nametable_queue_overflow]
-    and a
-    ret nz
-
-    ld a, [nes_nametable_queue_ptr_hi]
-    cp $E0
-    jr nc, .overflow
-
-    push hl
-    ld d, a
-    ld a, [nes_nametable_queue_ptr_lo]
-    ld e, a
-
-    pop hl
-    ld a, l
-    ld [de], a
-    inc de
-    ld a, h
-    ld [de], a
-    inc de
-
-    ld a, e
-    ld [nes_nametable_queue_ptr_lo], a
-    ld a, d
-    ld [nes_nametable_queue_ptr_hi], a
-    ret
-
-.overflow:
-    ld a, $01
-    ld [nes_nametable_queue_overflow], a
-    ret
 
 nes_ppu_get_addr_hl:
     ld a, [nes_ppu_addr_hi]
